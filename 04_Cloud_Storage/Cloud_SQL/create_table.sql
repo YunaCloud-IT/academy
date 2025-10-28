429572358b95a6e8cdc5167436928ab574e6f78c
Patrick Eichler <patrick.eichler@yunacloud.com>
2025-10-28 13:45:13 +0100

    cloud sql commands

---
+++ 04_Cloud_Storage/Cloud_SQL/create_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE students (
+  student_id INTEGER,
+  student_name VARCHAR(100),
+  student_gpa NUMERIC(5,3),
+  student_enrolled_date DATE,
+  student_graduated BOOLEAN
+);
---
+++ 04_Cloud_Storage/Cloud_SQL/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO students (student_id, student_name, student_gpa, student_enrolled_date, student_graduated)
+VALUES
+    (102, 'Bob Johnson', 2.500, '2022-01-15', true),
+    (103, 'Charlie Brown', 3.125, '2024-09-01', false),
+    (104, 'Diana Prince', 4.000, '2023-09-01', false);
